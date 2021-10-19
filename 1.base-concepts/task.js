4dbf2d4c34d62985c06f5cd14fbe4b4ab4eb07d1
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2021-10-19 15:32:02 +0800

    Add files via upload

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,26 @@
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
-  return arr; // array
+  
+  let D = b*b - 4*a*c;
+  
+
+  if (D < 0){
+    return false;
+    console.log("Решения уравнения нет")
+  };
+
+  
+if (D == 0) {
+  arr.push(-b/(2*a));
 }
+else if (D>0) {
+  arr.push((-b + Math.sqrt(d) )/(2a)); 
+  arr.push((-b - Math.sqrt(d) )/(2a));
+}
+return arr; // array
+}
+console.log(solveEquation(5,6,13));
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
