6959ec8ef27cdeddafc22b9f709db72ee7d8de75
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2021-10-28 14:07:51 +0800

    Add files via upload

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,21 +2,14 @@ function solveEquation(a, b, c) {
   let arr = [];
   // код для задачи №1 писать здесь
   
-  let D = b*b - 4*a*c;
+  let d = b*b - 4*a*c;
   
-
-  if (D < 0){
-    return false;
-    console.log("Решения уравнения нет")
-  };
-
-  
-if (D == 0) {
-  arr.push(-b/(2*a));
+if (d == 0) {
+  arr.push(-b/(2 * a));
 }
-else if (D>0) {
-  arr.push((-b + Math.sqrt(d) )/(2a)); 
-  arr.push((-b - Math.sqrt(d) )/(2a));
+else if (d>0) {
+  arr.push((-b + Math.sqrt(d) )/(2 * a)); 
+  arr.push((-b - Math.sqrt(d) )/(2 * a));
 }
 return arr; // array
 }
