79eac891eb22e24b4aec0c74a9cc9483389daeff
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2021-11-08 15:55:08 +0800

    Add files via upload

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,6 +19,25 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
+if (percent<=0) {
+return `Параметр <Процентная ставка> содержит неправильное значение ${percent}`;
+}
+if (contribution<0){
+  return `Параметр <Первоначальный взнос> содержит неправильное значение ${contribution}`;
+}
+if (amount<=0){
+  return `Параметр <Сумма кредита> содержит неправильное значение ${amount}`;
+}
+let restAmount = (amount - contribution);
 
+let date = new Date();
+let date2 = new Date();
+let month = date.getMonth();
+let month2 = date2.getMonth();
+let period = (date2 - date)*12 + ((month2 - month)+1);
+
+let monthPercent = (1 / 12 * percent); 
+let monthPayment = restAmount * (monthPercent + monthPercent / (((1 + monthPercent)^period) - 1));
+let totalAmount = (contribution + monthPayment * period);
   return totalAmount;
 }
