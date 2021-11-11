dab5a882765ec7fa7451471eed482a9a4bbbc1cc
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2021-11-11 09:38:37 +0800

    Add files via upload

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  if(arr1.length !== arr2.length) {
+    return false;
+  }
+  
+  return arr1.every(
+    (_, i) => arr1[i] === arr2[i]
+  )
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  let positive = arr.filter((el) => el >= 0);
+  let three = positive.filter((el) => el % 3 === 0);
+  resultArr = three.map((el) => el * 10);
 
   return resultArr; // array
 }
