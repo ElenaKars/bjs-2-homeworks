b613b2fd58d7b3a5ce7c24c1ccdd3e7445071a04
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2021-11-14 21:09:28 +0800

    Add files via upload

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark]; 
+  } else {
+    this.marks.push(mark)
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  marks.forEach(
+    (el) => {
+      this.addMark(el)
+    }
+  )
+}
+
+Student.prototype.getAverage = function() {
+  if (this.marks !== undefined) {
+    
+    let sum = this.marks.reduce((sum,current) => sum + current)
+
+    return sum / this.marks.length;
+  }
+};
+
+Student.prototype.exclude = function(reason) {
+      this.excluded = reason;
+      delete this.subject;
+      delete this.marks;
+    }
+
+const newStudent = new Student("Ivan", "male", 34);
+
+newStudent.setSubject('History')
+newStudent.addMarks(3, 3, 3)
+console.log(newStudent);
