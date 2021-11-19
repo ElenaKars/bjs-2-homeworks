bb559b33ab772e341d7135f0314aba1b8bfe401e
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2021-11-19 21:36:40 +0800

    решение задач

---
+++ 5.classes/task.js
@@ -0,0 +1,151 @@
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount, state = 100, type = null) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this._state = state;
+    this.type = type;
+  }
+  fix() {
+    this.state= this._state *  1.5
+  }
+
+  set state(num){
+    if(num < 0){
+      this._state= 0;
+      return;
+    }
+    
+    if(num > 100){
+      this._state = 100;
+      return;
+    }
+    
+    this._state = num
+  }
+  
+  get state(){
+    return this._state;
+  }
+}
+
+class Magazine extends PrintEditionItem { 
+  constructor(name, releaseDate, pagesCount, state) {
+    super(name, releaseDate, pagesCount, state, "magazine");
+ }
+}
+
+class Book extends PrintEditionItem { 
+  constructor(author, name, releaseDate, pagesCount, state) {
+    super(name, releaseDate, pagesCount, state, "book");
+    this.author = author;
+  }
+}
+class NovelBook extends Book {
+  constructor(author, name, releaseDate, pagesCount, state) {
+    super(author, name, releaseDate, pagesCount, state);
+    this.type = "novel";
+  }
+} 
+class FantasticBook extends Book {
+  constructor(author, name, releaseDate, pagesCount, state) {
+    super(author, name, releaseDate, pagesCount, state);
+    this.type = "fantastic";
+  }
+} 
+class DetectiveBook extends Book {
+  constructor(author, name, releaseDate, pagesCount, state) {
+    super(author, name, releaseDate, pagesCount, state);
+    this.type = "detective";
+  }
+}
+
+
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book){
+    if(book.state > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+    let result = this.books.find(function(book){
+      return book[type] === value;
+    })
+
+    return result || null;
+  }
+
+  giveBookByName(bookName){
+    const book = this.findBookBy('name', bookName);
+
+    if(!book) {
+      return null
+    }
+
+    let indOfBook = this.books.indexOf(book);
+    this.books.splice(indOfBook, 1);
+
+    return book
+  }
+}
+
+class Student {
+  constructor(name){
+    this.name = name;
+  }
+  setSubject(subjectName){
+    this.subject[subjectName] = [];
+  }
+  addMark(mark, subjectName) {
+    if (this.mark < 1 && this.mark > 5){
+      console.log("Ошибка, оценка должна быть числом от 1 до 5")
+    }
+    if(this.mark === undefined && this.subject[subjectName] === undefined){ 
+      this.subject[subjectName] = [mark]; 
+    } else {
+      this.subject[subjectName].push(mark)
+      this.marks.push(mark)
+    }
+  }
+  
+  addMarks(...marks,subjectName) {
+    this.subject[subjectName] = [...this.subject[subjectName], ...marks]
+  }
+  getAverageBySubject(){
+
+    if(this.subject(subjectName) === undefined){
+    return console.log("Предмет не существует")
+    }
+
+    let sub = this.subject(subjectName);
+    let sumMarks = 0;
+    
+
+    for(let item in sub){
+      item += sumMarks;
+    }
+    let avgBySub = sumMarks / sub.length;
+    return avgBySub
+  }
+
+  getAverage() {
+    if (this.marks !== undefined) {
+      
+      let sum1 = this.marks.reduce((sum,current) => sum + current)
+  
+      return sum1 / this.marks.length;
+    }
+  };
+  
+  exclude(reason) {
+        this.excluded = reason;
+        delete this.subject;
+        delete this.marks;
+      }
+  }
