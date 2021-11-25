c8fab66c73c43038a6c45acd03ae47aae9d247bc
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2021-11-25 23:38:37 +0800

    задачи решены

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,65 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = [];
+
+  return function (...args) {
+    const hash = args.join(',');
+
+    const idx = cache.findIndex((item)=> {
+      return item.hash === hash
+    });
+
+    if(idx !== -1) {
+      return `Из кэша: ${cache[idx].value}`
+    }
+
+    const result = func(...args);
+
+    cache.push({
+      hash,
+      value: result
+    })
+
+    if(cache.length > 5) {
+      cache.shift()
+    }
+
+    return `Вычисляем: ${result}`;
+  }
+}
+
+//задача №2
+function debounceDecoratorNew(func, ms) {
+  let isCooldown = false;
+
+  return function () {
+    if (isCooldown) return;
+
+    func.apply(this, arguments);
+
+    isCooldown = true;
+
+    setTimeout(() => (isCooldown = false), ms);
+  };
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+// задача №3
+function debounceDecorator2(func, ms) {
+  let isCooldown = false;
+
+  const wrapper = function () {
+    if (isCooldown) return;
+
+    func.apply(this, arguments);
+
+    wrapper.count += 1;
+
+    isCooldown = true;
+
+    setTimeout(() => (isCooldown = false), ms);
+  };
+
+  wrapper.count = 0
+  return wrapper
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
-}
